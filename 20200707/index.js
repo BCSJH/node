b95525988b9360fe84013b249e21518b27a213ae
BCSJH <kimbh666@naver.com>
2020-07-07 09:40:39 +0900

    20200707

---
+++ 20200707/index.js
@@ -0,0 +1,11 @@
+var express = require('express'); //설치한 express 모듈 불러와서 변수에 담기
+var app = express(); // express 실행해 app object 초기화
+
+app.get('/',function(req,res){ // '/' 위치에 'get' 요청 받을 경우
+   res.send('Hello World!'); //보내기
+});
+
+var port = 1234; //사용할 포트
+app.listen(port,function(){
+    console.log('server on! http://localhost:'+port);
+});
